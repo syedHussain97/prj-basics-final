ff89b17685d7257958a197cf19c1d53a6f5c5fa6
syedHussain97 <syed.m.husyn@gmail.com>
2020-05-28 22:04:50 +1200

    finishing routing practice

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,12 +9,12 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe(1, 'A Test Recipe', 'This is simply a test',
+    new Recipe(0, 'A Test Recipe', 'This is simply a test',
       'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20),
       ]),
-    new Recipe(2, 'Big freaking Burger ', 'This is simply a test :)',
+    new Recipe(1, 'Big freaking Burger ', 'This is simply a test :)',
       'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1),
