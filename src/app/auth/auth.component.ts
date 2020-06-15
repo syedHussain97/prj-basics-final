6b80213a09a17d38423ac0ca6147b1f40adfc512
Syed Hussain <syed.m.husyn@gmail.com>
2020-06-16 08:49:47 +1200

    adding form template to the form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+
+  }
+
+  onSubmitMethod(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
