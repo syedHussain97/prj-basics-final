4bb013cedb9d5c4db6def8b902213a8de2e4e993
Syed Hussain <syed.m.husyn@gmail.com>
2020-06-17 09:29:28 +1200

    adding fetch data for authenticated user

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,16 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
-import {map, tap} from 'rxjs/operators';
+import {exhaustMap, map, take, tap} from 'rxjs/operators';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
 
   constructor(private http: HttpClient,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes() {
@@ -18,8 +20,14 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>('https://learning-angular-http-request.firebaseio.com/recipes.json')
-      .pipe(map(recipes => {
+
+    return this.authService.user.pipe(take(1),
+      exhaustMap(user => {
+        return this.http.get<Recipe[]>('https://learning-angular-http-request.firebaseio.com/recipes.json', {
+          params: new HttpParams().set('auth', user.token)
+        });
+      }),
+      map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
         });
