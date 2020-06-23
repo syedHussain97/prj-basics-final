60fd51f3596a6df0e8c6c3a5d43e28f6461f7065
Syed Hussain <syed.m.husyn@gmail.com>
2020-06-23 15:20:08 +1200

    moving services and renaming shipping-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,19 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {DropdownDirective} from './shared/dropdown.directive';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {RecipeService} from './recipes/recipe.service';
+import {HttpClientModule} from '@angular/common/http';
 import {AuthComponent} from './auth/auth.component';
-import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {RecipesModule} from './recipes/recipes.module';
-import {ShoppingModule} from './shopping-list/shopping.module';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DropdownDirective,
     HeaderComponent,
-    AuthComponent,
-    LoadingSpinnerComponent
+    AuthComponent
   ],
   imports: [
     BrowserModule,
@@ -32,11 +27,10 @@ import {ShoppingModule} from './shopping-list/shopping.module';
     HttpModule,
     AppRoutingModule,
     RecipesModule,
-    ShoppingModule
+    ShoppingListModule,
+    SharedModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, {
-    provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true
-  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -3,15 +3,15 @@ import {ShoppingListComponent} from './shopping-list.component';
 import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
 import {AppRoutingModule} from '../app-routing.module';
 import {ShoppingListRoutingModule} from './shopping-list.routing.module';
-import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [ShoppingListComponent,
     ShoppingEditComponent],
-  imports: [AppRoutingModule, ShoppingListRoutingModule, CommonModule, FormsModule]
+  imports: [AppRoutingModule, ShoppingListRoutingModule, SharedModule, FormsModule]
 })
-export class ShoppingModule {
+export class ShoppingListModule {
 
 }
