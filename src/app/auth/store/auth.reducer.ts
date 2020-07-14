673cba2a7c8f250323d9f43e2a59f7140bd4f425
Syed Hussain <syed.m.husyn@gmail.com>
2020-07-15 09:02:47 +1200

    adding auth actions and adding to reducers

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import {User} from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
   user: User;
@@ -9,7 +10,25 @@ const initialState = {
 };
 
 
-export function authReducer(state = initialState, action) {
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+
+  switch (action.type) {
+
+    case AuthActions.LOGIN:
+      const user = new User(action.payload.email, action.payload.userId, action.payload.token, action.payload.expirationDate);
+      return {
+        ...state,
+        user: user
+      };
+
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      };
+    default:
+      return state;
+  }
 
   return state;
 }
