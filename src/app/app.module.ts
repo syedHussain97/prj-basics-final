fe8336ef69e272306faf74daa0f7cb351484ed37
syedHussain97 <syed.m.husyn@gmail.com>
2020-07-20 21:54:48 +1200

    minor changes with some plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -11,6 +13,7 @@ import {StoreModule} from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './auth/store/auth.effects';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -27,7 +30,9 @@ import {AuthEffects} from './auth/store/auth.effects';
     CoreModule,
     // AuthModule,
     StoreModule.forRoot(fromApp.appReducer),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({logOnly: environment.production})
   ],
   bootstrap: [AppComponent]
 })
